8bcd1bd8646888cdd5ad0c37a9b7ae85ca2f5ada
Gerard Cano <gercanbus@alu.edu.gva.es>
2025-08-07 16:41:11 +0200

    he creado dos alias

--- hellogitV3.py
+++ hellogitV3.py
@@ -1,2 +1,4 @@
 print("control de versiones con github")
 y ahora lo modifico desde otro ordenador remoto (en este caso desde Github)
+
+ahora he creado dos alias. Uno para ver el log (git lg) y otro para ver el log remoto (git lgr)
