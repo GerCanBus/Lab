946a9ec5062127bfdf271b4eda067c49a0ab9415
GerCanBus <GerCanBus@alu.edu.gva.es>
2025-08-07 15:42:26 +0200

    Resuelve conflictos después del pull

--- hellogit.py
+++ hellogit.py
@@ -2,4 +2,3 @@ print("¡¡aprendiendo a usar git!!")
 print("lo conseguí, ahora puedo hacer un commit")
 print("ahora estoy modificando el fichero")
 
-print("he tenido que usar el conmando 'git push --force origin main' porque fallaba la subida inicial")
--- hellogitV2.py
+++ hellogitV2.py
@@ -1,2 +1,4 @@
 print("este es un nuevo cambio")
 print("este es mi segundo commit")
+
+print("he tenido que usar el conmando 'git push --force origin main' porque fallaba la subida inicial")
