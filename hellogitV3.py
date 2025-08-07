c71b8b10963b01ce8faf23dc2dde9fc41f9f244f
GerCanBus <GerCanBus@alu.edu.gva.es>
2025-08-07 16:47:33 +0200

    ya he puesto el print

--- hellogitV3.py
+++ hellogitV3.py
@@ -1,4 +1,5 @@
 print("control de versiones con github")
-y ahora lo modifico desde otro ordenador remoto (en este caso desde Github)
 
-ahora he creado dos alias. Uno para ver el log (git lg) y otro para ver el log remoto (git lgr)
+print("Y ahora lo modifico desde otro ordenador remoto -en este caso desde Github-")
+
+print("ahora he creado dos alias. Uno para ver el log 'git lg' y otro para ver el log remoto 'git lgr'")
