71e0ee2dd43fd286e8f3056a421e9de7b429a293
GerCanBus <GerCanBus@alu.edu.gva.es>
2025-08-07 16:00:44 +0200

    lo voy entendiendo

--- hellogit.py
+++ hellogit.py
@@ -2,3 +2,4 @@ print("¡¡aprendiendo a usar git!!")
 print("lo conseguí, ahora puedo hacer un commit")
 print("ahora estoy modificando el fichero")
 
+print("Ya lo voy entendiendo. Nuedo hacer un add y un push porquer algo ha cambiado en el repositorio remoto") 
