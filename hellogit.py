d7eef255fe70d04a0e785f199b1fa70fca25dc4d
GerCanBus <GerCanBus@alu.edu.gva.es>
2025-08-07 16:16:20 +0200

    push

--- hellogit.py
+++ hellogit.py
@@ -3,3 +3,5 @@ print("lo conseguí, ahora puedo hacer un commit")
 print("ahora estoy modificando el fichero")
 
 print("Ya lo voy entendiendo. Nuedo hacer un add y un push porquer algo ha cambiado en el repositorio remoto") 
+
+print("Ahora estoy haciendo un nuevo cambio para ver si funciona el push")
